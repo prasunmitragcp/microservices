50904cef7c33f8ada9fcee566e17ddc06e549403
Prasun Mitra <pmitra@firstderivatives.com>
2022-04-13 13:18:12 +0100

    upload hystrix dashboard microservice code.

---
+++ src/main/java/com/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,15 @@
+package com.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+
+}
